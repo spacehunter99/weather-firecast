bb2836497fcdc8ccdd76149d2d7db7ab36bf792b
Michael <m.klepcha42@mail.ru>
2022-09-05 15:02:35 +0300

    protocol correct

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function getWeather() {
     let inputField = document.getElementById("chosenCity");
 
     let newUrl = encodeURIComponent(`${inputField.value}`);
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${newUrl}&appid=cdf1a7daa4674455386f14cadc943c0f&lang=ru&units=metric`);
+    let response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=${newUrl}&appid=cdf1a7daa4674455386f14cadc943c0f&lang=ru&units=metric`);
     let result = await response.json();
 
     city.innerHTML = "Город: " + result.name;
